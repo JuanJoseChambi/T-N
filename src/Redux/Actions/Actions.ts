2a9f94b0f45aaa182572ae6f0c2b1f955202329a
Juan Jose <chambijuanjose05@gmail.com>
2023-10-10 20:12:52 -0300

    ActionsRedux

--- src/Redux/Actions/Actions.ts
+++ src/Redux/Actions/Actions.ts
@@ -1,5 +1,6 @@
 import { createAction } from "@reduxjs/toolkit";
+import { Todo } from "../../Interfaces/interfaces";
 
-export const addTodo = createAction<string>("todos/addTodos")
+export const addTodo = createAction<Todo>("todos/addTodos")
 export const toggleTodo = createAction<string>("todos/toggleTodo")
 export const removeTodo = createAction<string>("todos/removeTodo")
