f3f330474f864f2a83cb1620cd198957efb32420
Juan Jose <chambijuanjose05@gmail.com>
2023-10-12 19:37:37 -0300

    RenderListToDo

--- src/Components/ToDoItem/ToDoItem.tsx
+++ src/Components/ToDoItem/ToDoItem.tsx
@@ -1,7 +1,12 @@
+import { Todo } from "../../Interfaces/interfaces"
+import style from "./ToDoItem.module.scss";
 
-function ToDoItem() {
+function ToDoItem({id, text, completed } : Todo) {
   return (
-    <div>ToDoItem</div>
+    <div key={id} className={style.itemTodo}>
+      <button className={style.checkTodo}>{completed ? "✔" : "❌"}</button>
+      <p className={`${!!completed ? style.textCompleted: style.incompleted}`}>{text}</p>
+    </div>
   )
 }
 
--- src/Components/ToDoList/ToDoList.tsx
+++ src/Components/ToDoList/ToDoList.tsx
@@ -1,20 +1,17 @@
 import style from "./ToDoList.module.scss";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Interfaces/interfaces";
+import ToDoItem from "../ToDoItem/ToDoItem";
 
 function ToDoList() {
   const { todos } = useSelector((state: RootState) => state.todos)
 
-  function loged () {
-    console.log(todos);
-    
-  }
-  
-
   return (
     <div className={style.viewToDoList}>
         <h1>To Do List</h1>
-        <button onClick={loged}>state</button>
+        {todos.map(todo => (
+          <ToDoItem key={todo.id} id={todo.id} text={todo.text} completed={todo.completed} />
+        ))}
     </div>
   )
 }
