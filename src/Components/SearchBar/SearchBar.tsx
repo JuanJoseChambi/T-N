da41fa4a06382d0512f3e402c9bda75bd6e1ed8f
Juan Jose <chambijuanjose05@gmail.com>
2023-10-15 20:30:31 -0300

    TodoListStyle

---
+++ src/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+import style from "./SearchBar.module.scss";
+
+function SearchBar() {
+    const inputSearch = useRef<HTMLInputElement | null>(null)
+    const divSearch = useRef<HTMLDivElement | null>(null)
+    const [searchClick, setSearchClick] = useState(false)
+
+    useEffect(() => {
+        const handleClickOutside = (event : MouseEvent) => {
+            if (divSearch.current && !divSearch.current.contains(event.target as Node)) {
+              // Si el clic ocurre fuera del div, establece divClicked en false
+              setSearchClick(false);
+            }
+          };
+          // Agrega un controlador de eventos al documento para detectar clics
+          document.addEventListener("click", handleClickOutside);
+      
+          return () => {
+            // Limpia el controlador de eventos cuando el componente se desmonta
+            document.removeEventListener("click", handleClickOutside);
+          };
+    },[])
+
+  return (
+    <div className={`${style.searchComponent} ${searchClick? style.divClicked:null}`} onClick={() => setSearchClick(!searchClick)} ref={divSearch}>
+        <input className={style.input} type="text" placeholder="Search To Do" ref={inputSearch}/> 
+        <label>🔍</label>
+    </div>
+  )
+}
+
+export default SearchBar
--- src/View/ToDo/ToDo.tsx
+++ src/View/ToDo/ToDo.tsx
@@ -1,13 +1,19 @@
-import CreateToDo from "../../Components/CreateToDo/CreateToDo";
+// import CreateToDo from "../../Components/CreateToDo/CreateToDo";
 import ToDoList from "../../Components/ToDoList/ToDoList";
+import SearchBar from "../../Components/SearchBar/SearchBar";
 import style from "./ToDo.module.scss";
 
 function ToDo() {
   return (
     <div className={style.viewToDo}>
       <div className={style.TodoList}>
-        <CreateToDo />
-        <ToDoList />
+        <h1 className={style.titleToDo}>To List</h1>
+        <SearchBar/>
+        <div>
+          <ToDoList />
+
+        </div>
+        {/* <CreateToDo /> */}
       </div>
     </div>
   );
